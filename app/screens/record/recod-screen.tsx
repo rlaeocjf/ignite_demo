fe94c5759001edb5cd0c877eb1ff6a2b939a9d11
김대철 <kimdaechul@gimdaecheol-ui-MacBookAir.local>
2022-08-09 22:42:04 +0900

    ios 녹음 기능 추가

--- app/screens/record/recod-screen.tsx
+++ app/screens/record/recod-screen.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect, FC } from "react"
-import { FlatList, TextStyle, View, ViewStyle, ImageStyle } from "react-native"
+import React, { useEffect, FC, useState } from "react"
+import { FlatList, TextStyle, View, ViewStyle, ImageStyle, Alert, Button } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
 import { Header, Screen, Text, AutoImage as Image, GradientBackground } from "../../components"
 import { color, spacing } from "../../theme"
 import { useStores } from "../../models"
 import { NavigatorParamList } from "../../navigators"
+import { Audio } from "expo-av"
+import { Recording } from "expo-av/build/Audio"
 
 const FULL: ViewStyle = {
   flex: 1,
@@ -45,6 +47,9 @@ const FLAT_LIST: ViewStyle = {
 export const RecordScreen: FC<StackScreenProps<NavigatorParamList, "demoList">> = observer(
   ({ navigation }) => {
     const goBack = () => navigation.goBack()
+    const [recording, setRecording] = useState<Recording>()
+    const [recordings, setRecordings] = useState([])
+    const [message, setMessage] = useState<string>("")
 
     const { characterStore } = useStores()
     const { characters } = characterStore
@@ -56,12 +61,80 @@ export const RecordScreen: FC<StackScreenProps<NavigatorParamList, "demoList">> 
       fetchData()
     }, [])
 
+    const startRecording = async () => {
+      try {
+        console.log("Requesting permissions..")
+        const permission = await Audio.requestPermissionsAsync()
+        console.log(permission)
+        if (permission.status === "granted") {
+          await Audio.setAudioModeAsync({
+            allowsRecordingIOS: true,
+            playsInSilentModeIOS: true,
+          })
+          console.log("Starting recording..")
+          const { recording } = await Audio.Recording.createAsync(
+            Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY,
+          )
+          setRecording(recording)
+          console.log("Recording started")
+        } else {
+          setMessage("please grant permission to app to access microphone")
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    const stopRecording = async () => {
+      console.log("Stopping recording..")
+      setRecording(undefined)
+      await recording.stopAndUnloadAsync()
+      const uri = recording.getURI()
+      console.log("Recording stopped and stored at", uri)
+
+      const updateRecordings = [...recordings]
+      const { sound, status } = await recording.createNewLoadedSoundAsync()
+
+      if (status.isLoaded) {
+        updateRecordings.push({
+          sound: sound,
+          duration: getDurationFormatted(status.durationMillis),
+          file: recording.getURI,
+        })
+      }
+      setRecordings(updateRecordings)
+    }
+
+    const getDurationFormatted = (millis: number) => {
+      const minutes = millis / 1000 / 60
+      const minutesDisplay = Math.floor(minutes)
+      const seconds = Math.round((minutes - minutesDisplay) * 60)
+      const secondsDisplay = seconds < 10 ? `0{seconds}` : seconds
+      return `${minutesDisplay}:${secondsDisplay}`
+    }
+
+    const getRecordingLines = () => {
+      return recordings.map((recordingLine, index) => {
+        return (
+          <View key={index}>
+            <Text>
+              Recording {index + 1} - {recordingLine.duration}
+            </Text>
+            <Button onPress={() => recordingLine.sound.replayAsync()} title="Play" />
+          </View>
+        )
+      })
+    }
+
     return (
       <View testID="RecordScreen" style={FULL}>
         <GradientBackground colors={["#422443", "#281b34"]} />
         <Screen style={CONTAINER} preset="fixed" backgroundColor={color.transparent}>
           <View>
-            <Text>abcd</Text>
+            <Text>{message}</Text>
+            <Button title="start recording" color="#841584" onPress={startRecording} />
+            <Button title="stop recording" color="#841584" onPress={stopRecording} />
+            {getRecordingLines()}
           </View>
         </Screen>
       </View>
