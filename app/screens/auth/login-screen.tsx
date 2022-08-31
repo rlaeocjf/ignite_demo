966635bb00ebd9fa8aa003accf83c7516af25f57
김대철 <kimdaechul@gimdaecheol-ui-MacBookAir.local>
2022-08-31 11:38:40 +0900

    로그인창 디자인 적용

--- app/screens/auth/login-screen.tsx
+++ app/screens/auth/login-screen.tsx
@@ -1,9 +1,8 @@
 import React, { FC, useContext, useState } from "react"
-import { Alert, TextInput, View, ViewStyle } from "react-native"
+import { Alert, Image, ImageBackground, View, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
-import { Button, Screen } from "../../components"
-import { color } from "../../theme"
+import { Button, Screen, Text } from "../../components"
 import { AuthContext, NavigatorParamList } from "../../navigators"
 import auth from "@react-native-firebase/auth"
 import styled from "styled-components/native"
@@ -11,12 +10,7 @@ import styled from "styled-components/native"
 const FULL: ViewStyle = {
   flex: 1,
 }
-const CONTAINER: ViewStyle = {
-  backgroundColor: color.transparent,
-  justifyContent: "center",
-  alignItems: "center",
-  flex: 1,
-}
+
 const errorMessages = {
   "auth/email-already-in-use": "이미 가입된 이메일입니다.",
   "auth/wrong-password": "잘못된 비밀번호입니다.",
@@ -52,50 +46,116 @@ export const LoginScreen: FC<StackScreenProps<NavigatorParamList, "login">> = ob
     }
     // 로그인
     const handleSignIn = () => {
-      auth()
-        .signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-          const user = userCredential.user
-          user
-            .getIdToken(false)
-            .then((data) => {
-              console.log(data)
-              signIn({ refreshToken: data })
-            })
-            .catch((err) => console.log(err))
-        })
-        .catch((error) => {
-          const alertMessage = errorMessages[error.code]
-            ? errorMessages[error.code]
-            : "알 수 없는 이유로 로그인에 실패했습니다."
-          console.log(error.code)
-          Alert.alert("로그인 실패", alertMessage)
-        })
+      // auth()
+      //   .signInWithEmailAndPassword(email, password)
+      //   .then((userCredential) => {
+      //     const user = userCredential.user
+      //     user
+      //       .getIdToken(false)
+      //       .then((data) => {
+      //         console.log(data)
+      //         signIn({ refreshToken: data })
+      //       })
+      //       .catch((err) => console.log(err))
+      //   })
+      //   .catch((error) => {
+      //     const alertMessage = errorMessages[error.code]
+      //       ? errorMessages[error.code]
+      //       : "알 수 없는 이유로 로그인에 실패했습니다."
+      //     console.log(error.code)
+      //     Alert.alert("로그인 실패", alertMessage)
+      //   })
+      signIn({ refreshToken: "temp" })
     }
 
     return (
       <View testID="LoginScreen" style={FULL}>
-        <Screen style={CONTAINER} preset="fixed">
+        <Container preset="fixed">
+          <LogoImage source={require("../../../assets/logo/logo2.jpeg")} />
           <InputWrap>
-            <TextInput placeholder="Email" value={email} onChangeText={(text) => setEmail(text)} />
-            <TextInput
+            <WelcomeWrap>
+              <WelcomeText text="Welcome back" size={30} color={"#000"} />
+              <WelcomeText text="Welcome back! Please enter your details." marginTop={10} />
+            </WelcomeWrap>
+            <LabelText>Email</LabelText>
+            <EmailInput placeholder="Email" value={email} onChangeText={(text) => setEmail(text)} />
+            <LabelText marginTop={20}>Password</LabelText>
+            <PasswordInput
               placeholder="Password"
               value={password}
               onChangeText={(text) => setPassword(text)}
               secureTextEntry
             />
+            <SigninBtn text="login" onPress={handleSignIn} />
+            <SignupWrap>
+              <SignupText text="Don't have an account?" marginRight={10} />
+              <SignupText text="Sign up" color={"#2999fe"} onPress={() => console.log(123)} />
+            </SignupWrap>
           </InputWrap>
-          <View>
-            <Button text="Login" onPress={handleSignIn} />
-          </View>
-        </Screen>
+        </Container>
       </View>
     )
   },
 )
 
-const InputWrap = styled.View`
-  background-color: red;
-  height: 200px;
+const Container = styled(Screen)`
   flex: 1;
+  padding: 20px;
+`
+const InputWrap = styled.View`
+  flex: 5;
+`
+const LogoImage = styled(Image)`
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+`
+const EmailInput = styled.TextInput`
+  border-radius: 7px;
+  padding: 10px;
+  border: 1px solid #e5e5e8;
+`
+const PasswordInput = styled.TextInput`
+  border-radius: 7px;
+  padding: 10px;
+  border: 1px solid #e5e5e8;
+`
+const SignupWrap = styled.View`
+  margin-top: 40px;
+  justify-content: center;
+  flex-direction: row;
+`
+const LabelText = styled.Text<{ marginTop?: number }>`
+  font-weight: 500;
+  text-align: left;
+  margin-bottom: 5px;
+  ${({ marginTop }) => marginTop && `margin-top:${marginTop}px`}
+`
+const SigninBtn = styled(Button)`
+  margin-top: 20px;
+  padding: 12px;
+  border-radius: 7px;
+  background-color: #2999fe;
+`
+const SignupText = styled(Text)<{ marginRight?: number; color?: string }>`
+  font-size: 15px;
+  ${({ marginRight }) => marginRight && `margin-right:${marginRight}px`}
+  color: ${({ color }) => (color ? `${color}` : "#adb1b6")};
+`
+const WelcomeWrap = styled.View`
+  margin: 10px 0px 30px 0;
+  justify-content: center;
+  align-items: center;
+`
+
+const WelcomeText = styled(Text)<{
+  marginTop?: number
+  color?: string
+  size?: number
+  weight?: number
+}>`
+  font-size: ${({ size }) => (size ? `${size}px` : "15px")};
+  font-weight: ${({ weight }) => (weight ? `${weight}` : "400")};
+  ${({ marginTop }) => marginTop && `margin-top:${marginTop}px`}
+  color: ${({ color }) => (color ? `${color}` : "#adb1b6")};
 `
